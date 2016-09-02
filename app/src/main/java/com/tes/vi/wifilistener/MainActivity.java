f38ea6f7e5b0125791dd6171eb353b565a5c9f08
Taufiqotul Faidah <taufiqotulfaidah@Amis-iPhone.icehousecorp.local>
2016-09-02 14:29:16 +0700

    read wifi ssid

--- app/src/main/java/com/tes/vi/wifilistener/MainActivity.java
+++ app/src/main/java/com/tes/vi/wifilistener/MainActivity.java
@@ -1,7 +1,10 @@
 package com.tes.vi.wifilistener;
 
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv_hello = (TextView)findViewById(R.id.tv_hello);
+
+        //String ssid = getIntent().getExtras().getString("ssid");
+        //tv_hello.setText(ssid);
     }
+
+
 }
--- app/src/main/java/com/tes/vi/wifilistener/MyWifiReceiver.java
+++ app/src/main/java/com/tes/vi/wifilistener/MyWifiReceiver.java
@@ -3,6 +3,9 @@ package com.tes.vi.wifilistener;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MyWifiReceiver extends BroadcastReceiver {
@@ -11,22 +14,35 @@ public class MyWifiReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        //throw new UnsupportedOperationException("Not yet implemented");
 
         String status = NetworkUtil.getConnectivityStatusString(context);
 
         Toast.makeText(context, status, Toast.LENGTH_LONG).show();
 
         if(status.equals("Wifi enabled")){
-            startActivity(context);
+
+            getSSID(context);
+
+            startActivity(context,"tes");
         }
     }
 
-    private void startActivity(Context context){
+    private void startActivity(Context context,String ssid){
         Intent i = new Intent(context, MainActivity.class);
         i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //i.putExtra("ssid",ssid);
         context.startActivity(i);
+    }
+
+    public void getSSID(Context context){
+        WifiManager wifiManager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
+        String wInfo =" wifiInfo : " + wifiInfo.toString();
+        String ssid = " SSID  : " + wifiInfo.getSSID();
+
+        Toast.makeText(context, wInfo + "\n" + ssid, Toast.LENGTH_LONG).show();
+        //Log.i("INFO", wInfo + "\n" + ssid);
+        //startActivity(context,ssid+ "\n" + wInfo);
     }
 }
